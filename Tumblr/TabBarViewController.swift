daf63e90d6c082cec30e13485a34bdc32574ce72
Marc Haumann <marchaumann@gmail.com>
2016-02-28 16:47:50 -0800

    initial setup

---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Marc Haumann on 2/27/16.
+//  Copyright © 2016 Marc Haumann. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
